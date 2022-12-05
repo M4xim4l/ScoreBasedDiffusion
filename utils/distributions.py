ad0028b2beddaa7a0cd5c4ba72d01ed28e658108
Max Augustin <s9mxaugu@stud.uni-saarland.de>
2022-12-05 17:01:32 +0100

    Initial commit

---
+++ utils/distributions.py
@@ -0,0 +1,33 @@
+import torch
+from torch.distributions import MultivariateNormal
+
+class SumOfGaussians:
+    def __init__(self, means, covariances, weights):
+        self.means = means
+        self.covariances = covariances
+        self.weights = torch.FloatTensor(weights)
+        self.cum_weights = torch.cat([torch.zeros(1),torch.cumsum(self.weights, dim=0)])
+
+
+        self.multivar_norms = []
+        for mean, covar in zip(means, covariances):
+            self.multivar_norms.append(MultivariateNormal(mean, covar)
+)
+    def log_prob(self, x):
+        return torch.log(self.prob(x))
+
+    def prob(self, x):
+        p_x = sum([w * distribution.log_prob(x).exp() for w, distribution in zip(self.weights, self.multivar_norms)])
+        return p_x
+
+    def sample(self, N):
+        rand_vals = torch.rand(N)[:,None]
+        rand_vals_minus_cum_weights = rand_vals - self.cum_weights[None,:]
+        larger_than_prev = (rand_vals_minus_cum_weights > 0)[:, :-1]
+        smaller_than_next = (rand_vals_minus_cum_weights <= 0)[:, 1:]
+        distribution_idcs = larger_than_prev & smaller_than_next
+        assert torch.sum(distribution_idcs.view(-1)) == N
+        samples = torch.zeros((N, len(self.means[0])))
+        for dist_idx, dist in enumerate(self.multivar_norms):
+            samples += distribution_idcs[:, dist_idx][:, None] * dist.sample((N,))
+        return samples
---
+++ utils/plotting_utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_3d_hist(ax, data, x_min, x_max, y_min, y_max, n_bins=50):
+    data_np = data.detach().numpy()
+    hist, xedges, yedges = np.histogram2d(data_np[:,0], data_np[:,1], bins=n_bins, range=[[x_min, x_max], [y_min, y_max]])
+    # Construct arrays for the anchor positions of the 16 bars.
+    xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
+    xpos = xpos.ravel()
+    ypos = ypos.ravel()
+    zpos = 0
+
+    # Construct arrays with the dimensions for the bars.
+    dx = dy = 0.5 * np.ones_like(zpos)
+    dz = hist.ravel()
+
+    #https://stackoverflow.com/questions/44895117/colormap-for-3d-bar-plot-in-matplotlib-applied-to-every-bar
+    max_height = np.max(dz)
+    min_height = np.min(dz)
+
+    cmap = plt.cm.get_cmap('plasma')
+    rgba = [cmap((k - min_height) / max_height) for k in dz]
+
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, zsort='average', color=rgba)
+
+
+def create_3d_hist_flat(ax, data, x_min, x_max, y_min, y_max, n_bins=50):
+    data_np = data.detach().numpy()
+    hist, xedges, yedges = np.histogram2d(data_np[:,0], data_np[:,1], bins=n_bins, range=[[x_min, x_max], [y_min, y_max]])
+    ax.imshow(hist, cmap='plasma')
+    ax.set_xticks([])
+    ax.set_yticks([])
